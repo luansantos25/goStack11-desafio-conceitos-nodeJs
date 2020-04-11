ca313c864a16d4fe1109bfb8e1ef44a07248f013
Luan <luan.dev.santos@gmail.com>
2020-04-11 16:26:01 -0300

    Add validateRepositoryId middleware

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -9,6 +9,19 @@ app.use(express.json());
 app.use(cors());
 
 const repositories = [];
+
+function validateRepositoryId(request, response, next) {
+  const { id } = request.params
+  
+  if(!isUuid(id)) {
+    return response.status(400).json({error: 'Invalid repository ID.'})
+  }
+  
+  next()
+}
+
+app.use('/repositories/:id', validateRepositoryId)
+
 
 app.get("/repositories", (request, response) => {
   return response.json(repositories)
